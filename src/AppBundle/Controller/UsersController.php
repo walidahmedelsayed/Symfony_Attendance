a22cdc081e5cf76afe4282bec899233754266175
walidahmedelsayed <walidahmedelsayed1@gmail.com>
2017-05-17 16:42:35 +0200

    Return Object In User Creation

--- src/AppBundle/Controller/UsersController.php
+++ src/AppBundle/Controller/UsersController.php
@@ -59,7 +59,12 @@ class UsersController extends FOSRestController
         $em = $this->getDoctrine()->getManager();
         $em->persist($data);
         $em->flush();
-        return new View($data, Response::HTTP_OK);
+        $object = new stdClass();
+        $object->id = $data->getId();
+        $object->name = $data->getName();
+        $object->track = $data->getTrack();
+        $object->type = $data->getType();
+        return new View($object, Response::HTTP_OK);
     }
 
     /**
